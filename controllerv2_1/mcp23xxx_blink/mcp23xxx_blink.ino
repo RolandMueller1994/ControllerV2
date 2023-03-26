0b87f5f53dd68ca0fea9c0e94660a8defff78e51
Roland Mueller <roland.mueller.1994@freenet.de>
2023-03-26 16:05:09 +0200

    Added amp schematic

--- controllerv2_1/mcp23xxx_blink/mcp23xxx_blink.ino
+++ controllerv2_1/mcp23xxx_blink/mcp23xxx_blink.ino
@@ -5,14 +5,15 @@
 #include "Adafruit_MCP23X08.h"
 #include "Adafruit_MCP23X17.h"
 
-#define LED_PIN 0     // MCP23XXX pin LED is attached to
-
+#define LED_PIN 9     // MCP23XXX pin LED is attached to
+#define LED_PIN_2 8 
 // only used for SPI
 #define CS_PIN 6
 
 // uncomment appropriate line
-Adafruit_MCP23X08 mcp;
-//Adafruit_MCP23X17 mcp;
+//Adafruit_MCP23X08 mcp;
+Adafruit_MCP23X17 mcp;
+Adafruit_MCP23X17 mcp2;
 
 void setup() {
   pinMode(6, OUTPUT);
@@ -27,15 +28,25 @@ void setup() {
   Serial.println("MCP23xxx Blink Test!");
   
   // uncomment appropriate mcp.begin
-  if (!mcp.begin_I2C(0x23)) {
+  if (!mcp.begin_I2C(0x20)) {
   //if (!mcp.begin_SPI(CS_PIN)) {
     Serial.println("Error.");
+    while (1);
+  }
+  if (!mcp2.begin_I2C(0x20, &Wire1)) {
+  //if (!mcp.begin_SPI(CS_PIN)) {
+    Serial.println("Error Wire1.");
     while (1);
   }
 
   // configure pin for output
   Serial.println(mcp.pinMode(LED_PIN, OUTPUT));
+  Serial.println(mcp.pinMode(LED_PIN_2, OUTPUT));
   Serial.println(mcp.readIoDir());
+
+    Serial.println(mcp2.pinMode(LED_PIN, OUTPUT));
+    Serial.println(mcp2.pinMode(LED_PIN_2, OUTPUT));
+    Serial.println(mcp2.readIoDir());
 
   Serial.println("Looping...");
 }
@@ -43,7 +54,19 @@ void setup() {
 void loop() {
   
   Serial.println(mcp.digitalWrite(LED_PIN, 1));
+  Serial.println(mcp.digitalWrite(LED_PIN_2, 1));
+  Serial.println(mcp2.digitalWrite(LED_PIN, 1));
+  Serial.println(mcp2.digitalWrite(LED_PIN_2, 1));
+  delay(500);
+  Serial.println(mcp2.digitalWrite(LED_PIN, 0));
+  Serial.println(mcp2.digitalWrite(LED_PIN_2, 0));
   delay(500);
   Serial.println(mcp.digitalWrite(LED_PIN, 0));
+  Serial.println(mcp.digitalWrite(LED_PIN_2, 0));
+  Serial.println(mcp2.digitalWrite(LED_PIN, 1));
+  Serial.println(mcp2.digitalWrite(LED_PIN_2, 1));
+  delay(500);
+  Serial.println(mcp2.digitalWrite(LED_PIN, 0));
+  Serial.println(mcp2.digitalWrite(LED_PIN_2, 0));
   delay(500);
 }
